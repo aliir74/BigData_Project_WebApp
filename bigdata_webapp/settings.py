189c075d192320990d7777eccf15dc350b7ae829
Ali Irani <ali.irani@cafebazaar.ir>
2020-07-30 16:32:19 +0430

    Add settings.json file for settings

--- bigdata_webapp/settings.py
+++ bigdata_webapp/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import json
+
+with open('settings.json') as json_file:
+    SETTINGS_FILE = json.load(json_file)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+txqmoz=k61v7zc%zqc9jyhm($c&-5p^2sstuwaohw9$27%+ed'
+SECRET_KEY =SETTINGS_FILE['secret_key']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
