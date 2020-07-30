4afe995ab2f0b1ebd4565d8dcb2fa5d21fb63fb4
Ali Irani <ali.irani@cafebazaar.ir>
2020-07-30 18:01:36 +0430

    Add get def in redis interface

--- webapp/redis_interface.py
+++ webapp/redis_interface.py
@@ -1,13 +1,61 @@
 import redis
 from django.conf import settings
+from datetime import datetime, timedelta
 
 redis_client = redis.Redis(host=settings.REDIS_SETTINGS['host'], port=settings.REDIS_SETTINGS['port'])
 
 
 class RedisInterface:
 
-    def __init__(self):
-        pass
+    @staticmethod
+    def get_post_in_6hours(user):
+        ans = 0
+        for i in range(6):
+            key = '{}${}'.format(user, (datetime.now()-timedelta(hours=i)).isoformat(timespec='hours'))
+            ans += redis_client.get(key)
+        return ans
+
+    @staticmethod
+    def get_post_in_1days():
+        ans = 0
+        for i in range(24):
+            key = 'post${}'.format((datetime.now() - timedelta(hours=i)).isoformat(timespec='hours'))
+            ans += redis_client.get(key)
+        return ans
+
+    @staticmethod
+    def get_unique_hashtags_in_1hour():
+        key = 'cnt$hashtags${}'.format((datetime.now()).isoformat(timespec='hours'))
+        ans = redis_client.get(key)
+        return ans
+
+    @staticmethod
+    def get_last_hashtags():
+        key = 'last$hashtags'
+        ans = redis_client.get(key)
+        return ans
+
+    @staticmethod
+    def get_last_posts():
+        key = 'last$posts'
+        ans = redis_client.get(key)
+        return ans
+
+    @staticmethod
+    def get_post_count_for_namad(namad):
+        _24hour = 0
+        _6hour = 0
+        _1hour = 0
+        for i in range(24):
+            key = '{}${}'.format(namad, (datetime.now() - timedelta(hours=i)).isoformat(timespec='hours'))
+            value = redis_client.get(key)
+            if i == 0:
+                _1hour = value
+            if i < 6:
+                _6hour += value
+            _24hour += value
+
+        return {1: _1hour, 6: _6hour, 24: _24hour}
 
     @staticmethod
     def update_keys(post):
